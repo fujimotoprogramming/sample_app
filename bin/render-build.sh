d683620f3f4f7dfda56417bad5f967cedd9a6054
fujimotoprogramming <fujimotoprogramming@example.com>
2023-08-13 10:19:34 +0000

    Add account activation

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-bundle exec rails db:migrate:reset
-bundle exec rails db:seed
+# Renderでエラーが出たため一旦コメントアウトしてます
+# bundle exec rails db:migrate:reset
+# bundle exec rails db:seed
