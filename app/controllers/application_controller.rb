f1fa67b0a20ba6993cff28b94c5fef7d5b3f2810
fujimotoprogramming <fujimotoprogramming@example.com>
2023-08-19 05:16:40 +0000

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ include SessionsHelper
   #def hello
   #  render html: "hello, world!"
   #end
+
+  private
+
+    # ユーザーのログインを確認する
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
 end
